02a4b25e42210e416a6c05fc70a9f5818d2bc6ff
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-05-24 22:19:10 +1000

    i need to test this live

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Scroll reveal
-
+/*
 function changeWidth() {
     var scroll = (window.pageYOffset / 5);
     var width = scroll;
@@ -10,4 +10,19 @@ function changeWidth() {
 
   window.addEventListener('scroll', function(){
     requestAnimationFrame(changeWidth);
-  })
+  }) */
+
+  function setup(){
+    createCanvas(innerWidth, innerHeight)
+  }
+
+  function draw (){
+    background ('red')
+    strokeWeight (5)
+    stroke ('black')
+    fill ('transparent')
+    rect (innerWidth/10, innerHeight/3, innerWidth/5*4, innerHeight/3)
+
+
+  }
+
